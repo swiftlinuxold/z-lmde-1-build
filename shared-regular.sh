c1ecd9850b4b186c62c2cbb02fcf7e13de610c2f
Jason Hsu <github@jasonhsu.com>
2012-01-14 23:58:16 -0600

    2012-01-14 23:58

--- shared-regular.sh
+++ shared-regular.sh
@@ -45,6 +45,7 @@ python $DIR_DEVELOP/ui-config/main.py
 
 # python $DIR_DEVELOP/1-build/cosmetic-regular.py
 
+python $DIR_DEVELOP/remove-languages/main.py # Must come second from last
 sh $DIR_DEVELOP/1-build/final.sh # MUST come last
 
 exit 0
