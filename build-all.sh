0243f701548e29e52010a7ac880f0eae22d11346
Jason Hsu <github@jasonhsu.com>
2012-03-27 01:24:49 -0500

    Suppressed part of build-all.sh

--- build-all.sh
+++ build-all.sh
@@ -12,5 +12,10 @@ fi
 UNAME=$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)
 DIR_DEVELOP=/home/$UNAME/develop
 
-sh $DIR_DEVELOP/1-build/build-regular.sh
-sh $DIR_DEVELOP/diet/create-diet.sh
+# sh $DIR_DEVELOP/1-build/build-regular.sh
+# sh $DIR_DEVELOP/diet/create-diet.sh
+
+mkdir $DIR_DEVELOP/temp-output
+
+# python $DIR_DEVELOP/special/build.py 'ts' 'Taylor Swift' | tee -a $DIR_DEVELOP/temp-output/build-ts.txt
+python $DIR_DEVELOP/special/build.py 'ts' 'Taylor Swift'
