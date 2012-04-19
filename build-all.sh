4e61d1871dd5d6c30c8ec594501fdefb7d71b7b5
Jason Hsu <github@jasonhsu.com>
2012-04-18 21:19:35 -0500

    Updated build-all.sh

--- build-all.sh
+++ build-all.sh
@@ -12,8 +12,9 @@ fi
 UNAME=$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)
 DIR_DEVELOP=/home/$UNAME/develop
 
-# sh $DIR_DEVELOP/1-build/build-regular.sh
-# sh $DIR_DEVELOP/diet/create-diet.sh
-
-
-python $DIR_DEVELOP/special/build.py 'ts' 'Taylor Swift'
+python $DIR_DEVELOP/1-build/build-regular.py
+python $DIR_DEVELOP/diet/build.py
+python $DIR_DEVELOP/special/build.py 'ts' 'Taylor Swift Linux'
+python $DIR_DEVELOP/special/build.py 'mn' 'Minnesota Swift Linux'
+python $DIR_DEVELOP/special/build.py 'chi' 'Chicago Swift Linux'
+python $DIR_DEVELOP/special/build.py 'sv' 'Silicon Valley Swift Linux'
